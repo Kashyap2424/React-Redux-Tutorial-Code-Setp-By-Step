6e7d68dc58ff04947286143c5604c6a58360db94
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-06-16 15:28:56 +0530

    Complete react-redux files & folder structure in proper way.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import "./App.css";
-import User from "./Components/User";
+import Home from "./Components/Home";
 
 function App() {
-  const userName = {
-    name: "John",
-    age: 30,
-  };
-
   return (
     <div className="App">
       <h1>Hello, React-Redux Tutorial</h1>
-      <User userName={userName} />
+      <Home />
     </div>
   );
 }
